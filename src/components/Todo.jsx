6a5327ec22f7a6f7dcd78ef4a9a5625246b2a8a0
Butterfly1411 <sabohattoshpolatova72@gmail.com>
2025-07-25 15:12:45 +0500

    added react-select

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,11 @@
 import React, { use, useState } from 'react'
+import Select from 'react-select'
 
 const Todo = () => {
     const [todos, setTodos] = useState([]); //useState bilan boshlangich qiymat array bilan 
     const [input, setInput] = useState(""); // inputdagi keladigan topshiriq
     const [filter, setFilter] = useState("all");
+    const [title, setTitle] = useState('');
 
     const addTodo = () => { // addTodo function
         if (input.trim()) { // agar inputdagi matnni trim qiladi
@@ -13,6 +15,10 @@ const Todo = () => {
                 completed: false // vazifa bajarilmagan
             }])
             setInput("") //foydalanuvchi matn yozislganda tozalaydi
+
+            if (title.trim() === '') return;
+            setTodos([...todos, title]);
+            setTitle('');
         }
     }
     const filterTodos = todos.filter((todo) => { // ozgaruvchi todosni filter todo qilib beradi
@@ -20,22 +26,24 @@ const Todo = () => {
         if (filter === "incompleted") return !todo.completed//agar filter incomplted qtiy tenglanadi qaytardi inkorda todo completed ni 
         return true; //true qaytaradi
     })
+    const options = [
+        { value: 'all', label: 'All' },
+        { value: 'completed', label: 'Completed' },
+        { value: 'incompleted', label: 'Incompleted' }
+    ]
 
     return (
         <div className='min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-600 to-emerald-400'>
             <div className="bg-white shadow-lg rounded-3xl p-16">
                 <h1 className='text-3xl font-bold text-center text-gray-900 mb-6 '>REACT TODO LIST </h1>
-                <div className='flex justify-between'>
+                <div className='flex justify-between items-center'>
                     <i class="bi bi-journal-text text-3xl text-blue-600"></i>
-                    <select value={filter} onChange={(e) => setFilter(e.target.value)}
-                        className='ml-50 mb-5 bg-gradient-to-r from-blue-600 to-emerald-400 p-2 rounded-[10px] text-gray-800'>
-                        <option className='bg-blue-200' value="all">All</option>
-                        <option className='bg-blue-200' value="completed">Complete</option>
-                        <option className='bg-blue-200' value="incomplete">Incomplete</option>
-                    </select>
+                    <Select value={filter} onChange={(e) => setFilter(e.value)} className='m-[10px]' options={options} />
                 </div>
                 <div className="mb-4 flex">
-                    <input value={input} onChange={(e) => setInput(e.target.value)}
+                    <input value={input} onChange={(e) => setInput(e.target.value)} onKeyUp={(e) => {
+                        if (e.key === 'Enter') addTodo();
+                    }}
                         //  {/* onchange foydalanuvchi yozganda boladigan hodisa e-event e.target-hodisa yuz berganda qiymati setinputga tenglanadi*/}
                         type="text" placeholder='Add a new todo ' className='flex-grow px-3 py-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500' />
                     <button onClick={addTodo} className='bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600'>Add</button>
@@ -43,7 +51,7 @@ const Todo = () => {
                 <ul>
                     {
                         filterTodos.map((todo) => (
-                            <li key={todo.id} className='flex items-center p-3 rounded-lg bg-slate-100 border border-gray-200'>
+                            <li key={todo.id} className='flex items-center p-3 rounded-lg bg-slate-100 border border-gray-200 mt-[5px]'>
                                 <input type="checkbox" checked={todo.completed}
                                     // {/* agar todo.complete===true bolsa ishlaydi*/} 
                                     onChange={() => setTodos(todos.map((t) => (t.id === todo.id ? { ...t, completed: !t.completed } : t)))}
